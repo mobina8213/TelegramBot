9409420194afd15da00d954f5b051c7918b97262
mobina <mobinaroshan99@gmail.com>
2025-06-09 01:04:49 +0330

    ایجاد فایل اولیه بات با پیام خوش‌آمدگویی ساده

---
+++ bot.py
@@ -0,0 +1,17 @@
+import os
+from dotenv import load_dotenv
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+load_dotenv()
+TOKEN = os.getenv("TOKEN")
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    print("دستور /start دریافت شد ✅")
+    await update.message.reply_text("سلام! خوش آمدید به ربات ما.")
+
+app = ApplicationBuilder().token(TOKEN).build()
+app.add_handler(CommandHandler("start", start))
+
+print("🚀 Bot is running...")
+app.run_polling()
