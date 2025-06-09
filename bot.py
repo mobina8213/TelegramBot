67d376f0b7f8c74de9c6298867c95625587066c6
mobina <mobinaroshan99@gmail.com>
2025-06-09 16:20:59 +0330

    افزودن دستور /help برای نمایش راهنمای استفاده از ربات

--- bot.py
+++ bot.py
@@ -12,8 +12,22 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 "سلام! خوش آمدید به ربات ما."
 "برای دیدن راهنمای دستورات، لطفاً از دستور /help استفاده کنید. 📋")
 
+async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    print("دستور /help دریافت شد ✅")
+    await update.message.reply_text(
+        "📌 راهنما:\n"
+        "/start - شروع به کار بات\n"
+        "/help - نمایش این راهنما\n"
+        "/about - درباره بات\n"
+        "/dataset - نمایش نمونه‌ای از دیتاست\n"
+        "/search <نام اپلیکیشن> - جستجوی اطلاعات اپ مورد نظر\n\n"
+        "مثال:\n/search Instagram"
+    )
+
+
 app = ApplicationBuilder().token(TOKEN).build()
 app.add_handler(CommandHandler("start", start))
+app.add_handler(CommandHandler("help", help_command))
 
 print("🚀 Bot is running...")
 app.run_polling()
